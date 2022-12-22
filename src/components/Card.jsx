c82f20072b85c3792b8d2374eaf4c281fd505ac1
Sunmeet Singh <renu.kabeer@gmail.com>
2022-12-22 20:58:22 +0530

    final prototype ready

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,27 @@
+import axios from 'axios'
 import React from 'react'
 import { cardContext } from './CardContainer'
 
-export const Card = ({identifier}) => {
-    const {cards, setCards} = React.useContext(cardContext)
+export const Card = ({ identifier, data }) => {
+    const { cards, setCards } = React.useContext(cardContext)
     const [isVisible, setIsVisible] = React.useState(true)
 
-    const handleDeletion = (card) => {
+    const handleDeletion = async (card) => {
         console.log(identifier)
         setIsVisible(false)
-        const newCards = cards.filter ((ele) => ele.key != identifier)
-        setTimeout(() => setCards (newCards), 850)
+        setTimeout(async () => {
+            await axios.delete(process.env.REACT_APP_BASE_URL + 'recruiter/delete-recruiter-flow-data/' + identifier)
+            window.location.reload()
+        }, 1000);
     }
     return (
         <div className='card animated bounce' style={{
             transition: 'all 1s',
             opacity: Number(isVisible),
-            margin: !isVisible? '-100px': '10px'
+            marginLeft: !isVisible ? '-100px' : ''
         }}>
             <div className="card-content">
-                Card content {identifier}
+                {data} {identifier}
                 <button onClick={(card) => handleDeletion(card)} >Delete</button>
             </div>
             <img src="https://picsum.photos/500/300?random=1" alt="" />
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react'
 import { Card } from './Card'
 export const cardContext = React.createContext(null);
 
 export const CardContainer = () => {
     const [cards, setCards] = useState([])
+    const getCards = async () => await axios.get(process.env.REACT_APP_BASE_URL + 'recruiter/get-recruiter-flow-data')
+
+    const handleAddition = useCallback(async () => {
+        await axios.post(process.env.REACT_APP_BASE_URL + 'recruiter/post-recruiter-flow-data')
+    })
+
+    useEffect(() => {
+        getCards().then((res) => setCards(res.data))
+    }, [handleAddition])
+
     return (
         <cardContext.Provider value={{ cards, setCards }}>
             <div className='mainContainer'>
-                <button onClick={() => setCards([...cards, <Card key={Date.now()} identifier={Date.now()} className='cardAdding' />])}>Add Card</button>
-                <div className='cardContainer'>{cards.map((card) => card)}</div>
+                <button onClick={handleAddition}>Add Card</button>
+                <div className="cardContainer">
+                    {cards.map((card) => <Card identifier={card._id} data={card.data} />)}
+                </div>
             </div>
         </cardContext.Provider>
     )
